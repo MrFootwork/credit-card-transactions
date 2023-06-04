db48cf42f465fc53dbe706f487383eb342515398
MrFootwork <pandau.ting@gmail.com>
2023-06-04 18:40:32 +0200

    stores setup => ready for UI coding

---
+++ src/components/CardSelector.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import CardSelectorBankingCard from "./CardSelectorBankingCard.vue";
+import { useCards } from "../store/cards";
+import { storeToRefs } from "pinia";
+
+const cardsStore = useCards();
+const { cards } = storeToRefs(cardsStore);
+// const { fetchCards } = cardsStore;
+</script>
+
+<template>
+  <div>
+    <div v-for="card in cards" :key="card.id">
+      Card here: {{ card.description }}
+      <CardSelectorBankingCard />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/CardsTransactions.vue
+++ src/views/CardsTransactions.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import { storeToRefs } from "pinia";
+import { onBeforeMount } from "vue";
+
 import { useCards } from "../store/cards";
 import { useTransactions } from "../store/transactions";
 import { useSelection } from "../store/selection";
+
+import CardSelector from "../components/CardSelector.vue";
 
 const cardsStore = useCards();
 const { cards } = storeToRefs(cardsStore);
@@ -15,11 +19,18 @@ const { fetchTransactions } = transactionsStore;
 const selectionStore = useSelection();
 const { cardId } = storeToRefs(selectionStore);
 const { selectCardByID } = selectionStore;
+
+onBeforeMount(async () => {
+  await fetchCards();
+  await fetchTransactions();
+});
 </script>
 
 <template>
   <div>
     <!-- FIXME add all needed components -->
+    <CardSelector></CardSelector>
+
     <p>selected card: {{ cardId }}</p>
     <button @click="selectCardByID('lkmfkl-mlfkm-dlkfm')">Select Card</button>
     <pre>{{ cards }}</pre>
