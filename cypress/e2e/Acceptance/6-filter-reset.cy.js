aa708bd4d717700c26e1f2557669de0e431f32ef
MrFootwork <pandau.ting@gmail.com>
2023-06-09 12:25:58 +0200

    🧪 all e2e test finished

--- cypress/e2e/Acceptance/6-filter-reset.cy.js
+++ cypress/e2e/Acceptance/6-filter-reset.cy.js
@@ -7,7 +7,27 @@ content should be reset`, () => {
     cy.visit('http://localhost:4000/');
   });
 
-  it(`resets the amount by selecting other card`, () => {
-    expect(true).to.be.false;
+  const cardTypes = ['private', 'business'];
+
+  cardTypes.forEach((cardType) => {
+    context(`change to ${cardType} card`, () => {
+      it(`resets the filter field`, () => {
+        // select first card
+        cy.get(`.wrapper.card.${cardType}`).click();
+        cy.get('#input-amount').type('123');
+        // reselect card
+        cy.get(`.wrapper.card.${oppositeOf(cardType)}`).click();
+        cy.get('#input-amount')
+          .invoke('val')
+          .then((text) => text.replace(',', '.'))
+          .then(parseFloat)
+          .should('equal', 0);
+      });
+    });
+    function oppositeOf(inputType) {
+      const oppositeIndex =
+        1 - cardTypes.findIndex((currentType) => currentType === inputType);
+      return cardTypes[oppositeIndex];
+    }
   });
 });
